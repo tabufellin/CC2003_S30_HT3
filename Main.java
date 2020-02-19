55cd7fd01f5344e2b63365f8c6c6fd120a1436e8
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-02-19 09:59:34 -0600

    Date update

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  *
  * @author Sebastian Gonzales (tabufellin) Pablo Ruiz (PingMaster99)
  * @version 1.0
- * @since 2020-02-04
+ * @since 2020-02-19
  **/
 public class Main {
     public static void main (String[] args) {
